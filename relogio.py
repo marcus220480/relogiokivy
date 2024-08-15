00b781ccd264fd30e274e70e9cd1a42113f6eb74
marcus220480 <72481190+marcus220480@users.noreply.github.com>
2024-08-15 11:28:04 -0300

    atualiza02

--- relogio.py
+++ relogio.py
@@ -14,14 +14,23 @@ class Relogio(MDApp):
             self.rect = Rectangle(size=self.layout.size, pos=self.layout.pos)
             self.layout.bind(size=self._update_rect, pos=self._update_rect)
 
-        self.label = Label(
+        self.data_label = Label(
+            text=self.get_date(),
+            font_size='70sp',
+            halign='center',
+            color=(1, 1, 1, 1)
+        )
+
+        self.hora_label = Label(
             text=self.get_time(),
             font_size='64sp',
             halign='center',
             color=(1, 1, 1, 1)
         )
 
-        self.layout.add_widget(self.label)
+        self.layout.add_widget(self.data_label)
+        self.layout.add_widget(self.hora_label)
+
         Clock.schedule_interval(self.update_time, 1)
         return self.layout
 
@@ -29,11 +38,15 @@ class Relogio(MDApp):
         self.rect.size = instance.size
         self.rect.pos = instance.pos
 
+    def get_date(self):
+        return datetime.now().strftime('%d/%m/%Y')
+
     def get_time(self):
         return datetime.now().strftime('%H:%M:%S')
 
     def update_time(self, *args):
-        self.label.text = self.get_time()
+        self.hora_label.text = self.get_time()
+        self.data_label.text = self.get_date()
 
 if __name__ == "__main__":
     Relogio().run()
